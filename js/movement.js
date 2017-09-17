5df10e2388dff07d78e4e9a60c324431e6668961
colorlessenergy <brianmunozdev@gmail.com>
2017-09-17 04:47:02 -0700

    jumping added

--- js/movement.js
+++ js/movement.js
@@ -21,17 +21,31 @@ class Movement {
       delete self.pressed[event.keyCode];
     });
 
-
+    if (this.vel.y === 0 && !(this.isDown(39))) {
+      animations.currentState = animations.standing;
+    }
 
     if (this.isDown(39)) {
-      this.pos.x += this.vel.x;
-      if (animations.frame % 5 === 0) {
-        animations.currentState = animations.walking[this.currentFrame];
-        console.log(animations.walking);
-        this.currentFrame++;
-        if (this.currentFrame > 2) {
-          this.currentFrame = 0;
+      if (this.vel.y < 0 || this.vel.y > 0) {
+        animations.currentState = animations.jumping;
+        this.pos.x += this.vel.x;
+      } else if (this.vel.y === 0){
+        this.pos.x += this.vel.x;
+        if (animations.frame % 5 === 0) {
+          animations.currentState = animations.walking[this.currentFrame];
+          console.log(animations.walking);
+          this.currentFrame++;
+          if (this.currentFrame > 2) {
+            this.currentFrame = 0;
+          }
         }
+      }
+    }
+
+    if (this.isDown(38)) {
+      if (this.vel.y === 0) {
+        animations.currentState = animations.jumping;
+        this.vel.y -= 12;
       }
     }
   }
