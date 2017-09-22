ad063a3b312a89eba658ea5b7e34b54b9996f054
colorlessenergy <brianmunozdev@gmail.com>
2017-09-22 06:08:27 -0700

    basic camera movement

--- js/game.js
+++ js/game.js
@@ -28,12 +28,25 @@ class Game {
 
 
   update () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.setTransform(1,0,0,1,0,0);//reset the transform matrix as it is cumulative
+    context.clearRect(0, 0, canvas.width, canvas.height);//clear the viewport AFTER the matrix is reset
+
+   //Clamp the camera position to the world bounds while centering the camera around the player
+   console.log(this.player.pos.x);
+   var camX = this.clamp(-this.player.pos.x + canvas.width / 2, -1000, 1000 - canvas.width);
+   var camY = this.clamp(-this.player.pos.y + canvas.height/2, 0, 0);
+
+    context.translate( camX, camY );
 
     window.requestAnimationFrame(this.update.bind(this));
 
     this.backgrounds.update();
     this.player.update();
+  }
+  clamp(value, min, max)  {
+    if(value < min) return min;
+    else if(value > max) return max;
+    return value;
   }
 }
 
